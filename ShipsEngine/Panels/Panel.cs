2d55f98d74d5badf414d17f57a708d93e53fb8c8
shipscaptainlll <shipscaptainlll@gmail.com>
2023-08-18 13:43:28 +0200

    0.0.2 - Panel Class Implementation
    Added a new Panel class to represent individual UI panels.
    Incorporated basic rendering of the panel using SFML's graphics library.
    Integrated the panel rendering into the main application loop in Program.cs.
    Initial positioning and appearance features added to the Panel class.

---
+++ ShipsEngine/Panels/Panel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace ShipsEngine.Panels
+{
+    internal class Panel
+    {
+        public string Title { get; private set; }
+        public Vector2f Position { get; set; }
+        public Vector2f Size { get; set; }
+        public Color Color { get; set; } = new Color(100, 100, 100);
+
+        public Panel(string title, Vector2f position, Vector2f size)
+        {
+            Title = title;
+            Position = position;
+            Size = size;
+        }
+
+        public Panel(string title, Vector2f position, Vector2f size, Color color)
+            : this (title, position, size)
+        {
+            Color = color;
+        }
+
+        public void Render(RenderWindow target)
+        {
+            RectangleShape shape = new RectangleShape(Size)
+            {
+                Position = this.Position,
+                Size = this.Size
+            };
+
+            target.Draw(shape);
+        }
+    }
+}
--- ShipsEngine/Program.cs
+++ ShipsEngine/Program.cs
@@ -1,9 +1,38 @@
-﻿
+﻿using ShipsEngine;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using SFML.Audio;
 
-public class Program
+
+namespace ShipsEngine
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        
+        static RenderWindow _window;
+
+        public static void Main(string[] args)
+        {
+
+            _window = new RenderWindow(new VideoMode(1080, 800), "GameEngine");
+            _window.Closed += OnClosed;
+
+            Panels.Panel panel = new Panels.Panel("HUD Panel", new SFML.System.Vector2f(500, 500), new SFML.System.Vector2f(100, 100));
+
+            while (_window.IsOpen)
+            {
+                _window.DispatchEvents();
+
+                _window.Clear();
+                panel.Render(_window);
+                _window.Display();
+            }
+        }
+
+        private static void OnClosed(object sender, System.EventArgs s)
+        {
+            _window.Close();
+            
+        }
     }
 }
